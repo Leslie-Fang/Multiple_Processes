c5c6799ece06ed55707f9d395a5a483948f4bf06
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-03-29 17:19:13 +0800

    init

---
+++ CMakeFiles/hwk4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwk4.dir/main.cpp.o"
+  "hwk4.pdb"
+  "hwk4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hwk4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
